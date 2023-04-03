d73b0629d9f2d7cbd7ba5765187fda10380eca16
IsaacShaker <114536281+IsaacShaker@users.noreply.github.com>
2023-04-03 11:52:39 -0400

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -10,7 +10,26 @@ const observer = new IntersectionObserver(entries => {
   });
   });
   
+  // // Another Observer (slide animation)
+  // const observer2 = new IntersectionObserver(entries => {
+  //   // Loop over the entries
+  // entries.forEach(entry => {
+  //   // If the element is visible
+  //   if (entry.isIntersecting) {
+  //     // Add the animation class
+  //     entry.target.classList.add('slide-animation');
+  //   }
+  // });
+  // });
+
   // Tell the observer which elements to track
   observer.observe(document.querySelector('#python'));
   observer.observe(document.querySelector('#javascript'));
   observer.observe(document.querySelector('#c'));
+
+  // observer2.observe(document.querySelector('#profile-pic'));
+  // observer2.observe(document.querySelector('#name-intro'));
+  // observer2.observe(document.querySelector('#intro-divider'));
+  // observer2.observe(document.querySelector('#filed-of-study'));
+
+
